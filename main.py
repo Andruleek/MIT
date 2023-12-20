50a733d3ba61434ee9797db257bf0204176eaeca
ExtymAndriy <150171957+ExtymAndriy@users.noreply.github.com>
2023-12-20 13:27:39 +0200

    first commit

---
+++ main.py
@@ -0,0 +1,67 @@
+from pathlib import Path 
+
+file_name = Path('.')
+
+for elem in file_name.glob('*JPEG'):
+    print(elem)
+
+for elem in file_name.glob('*PNG'):
+    print(elem)
+
+for elem in file_name.glob('*JPG'):
+    print(elem)
+
+for elem in file_name.glob('*SVG'):
+    print(elem)
+
+for elem in file_name.glob('*AVI'):
+    print(elem)
+
+for elem in file_name.glob('*MP4'):
+    print(elem)
+
+for elem in file_name.glob('*MOV'):
+    print(elem)
+
+for elem in file_name.glob('*MKV'):
+    print(elem)
+
+for elem in file_name.glob('*DOC'):
+    print(elem)
+
+for elem in file_name.glob('*DOCX'):
+    print(elem)
+
+for elem in file_name.glob('*TXT'):
+    print(elem)
+
+for elem in file_name.glob('*PDF'):
+    print(elem)
+
+for elem in file_name.glob('*XLSX'):
+    print(elem)
+
+for elem in file_name.glob('*PPTX'):
+    print(elem)
+
+for elem in file_name.glob('*MP3'):
+    print(elem)
+
+for elem in file_name.glob('*OGG'):
+    print(elem)
+
+for elem in file_name.glob('*WAV'):
+    print(elem)
+
+for elem in file_name.glob('*AMR'):
+    print(elem)
+
+for elem in file_name.glob('*ZIP'):
+    print(elem)
+
+for elem in file_name.glob('*GZ'):
+    print(elem)
+
+for elem in file_name.glob('*TAR'):
+    print(elem)
+
